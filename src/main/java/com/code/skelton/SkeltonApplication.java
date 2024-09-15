47a696ef103658d4ed9426d3fd241c5e935803c2
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-09-15 21:41:03 +0900

    chore: Spring bootの導入

---
+++ src/main/java/com/code/skelton/SkeltonApplication.java
@@ -0,0 +1,13 @@
+package com.code.skelton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeltonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeltonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/skelton/SkeltonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.skelton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeltonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
